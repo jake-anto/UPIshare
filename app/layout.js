96404a9c501a705865bb3c87b06fe2574b6ee37e
Jake Anto <64896514+j-eo@users.noreply.github.com>
2023-08-26 09:55:05 +0000

    Add inline css for faster load

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        style={{
+          margin: 0,
+        }}
+      >
+        {children}
+      </body>
     </html>
   );
 }
