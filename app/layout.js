ea7faddf6702138039ac3fe2c9d9bf9771c57385
Jake Anto <64896514+j-eo@users.noreply.github.com>
2023-08-26 20:34:06 +0000

    Add links to favicons

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,27 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff5722" />
+        <meta name="msapplication-TileColor" content="#ff5722" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body
         style={{
