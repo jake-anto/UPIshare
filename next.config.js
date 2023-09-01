eafaf052da2314ebd037f2d048a21f2b251e762c
Jake Anto <64896514+j-eo@users.noreply.github.com>
2023-09-01 20:55:06 +0000

    Automate the creation of service worker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-};
 
-module.exports = nextConfig;
+const { GenerateSW, InjectManifest } = require("workbox-webpack-plugin");
+
+module.exports = {
+  output: "export",
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(
+        new GenerateSW({
+          clientsClaim: true,
+          skipWaiting: true,
+          swDest: "sw.js",
+        })
+      );
+    }
+    return config;
+  },
+};
