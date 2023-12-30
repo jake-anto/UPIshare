d8a15e7617b5c5f54f67d2e5a1d0b0f1eb9790fe
Jake Anto <64896514+jake-anto@users.noreply.github.com>
2023-12-30 17:33:31 +0000

    Refactor form and add stepper component

--- app/page.js
+++ app/page.js
@@ -1,20 +1,123 @@
 "use client";
 
-import Header from "./header";
-import { AppShell } from "@mantine/core";
+import { AppShell, Button, Container, Flex, Stepper } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+import {
+  IconChevronRight,
+  IconForms,
+  IconShare3,
+  IconWand,
+} from "@tabler/icons-react";
+import { useState } from "react";
+import Customize from "./customize";
 import Form from "./form";
-import { Container } from "@mantine/core";
+import Header from "./header";
+import validate from "./validate";
 
 export default function Home() {
+  const [active, setActive] = useState(0);
+  const [formData, setFormData] = useState({
+    id: "",
+    name: "",
+    amount: "",
+    note: "",
+  });
+  const [errors, setErrors] = useState({
+    id: "",
+    name: "",
+    amount: "",
+    note: "",
+  });
+
+  const nextStep = () =>
+    setActive((current) => (current < 3 ? current + 1 : current));
+  const prevStep = () =>
+    setActive((current) => (current > 0 ? current - 1 : current));
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFormNumberInputChange = (name, value) => {
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = () => {
+    let errors = validate(formData);
+    setErrors(errors);
+
+    if (Object.values(errors).every((error) => error === "")) {
+      nextStep();
+    }
+  };
+
+  const small_screen = useMediaQuery("(max-width: 640px)");
+
   return (
     <>
-      <AppShell header={{ height: 50 }} padding="md">
+      <AppShell header={{ height: 72 }} padding="md">
         <AppShell.Header>
           <Header />
         </AppShell.Header>
         <AppShell.Main>
+          <Container size="sm" padding="sm" mb={12}>
+            <Stepper
+              active={active}
+              onStepClick={setActive}
+              wrap={false}
+              styles={{
+                stepBody: {
+                  display: small_screen ? "none" : "flex",
+                },
+              }}
+            >
+              <Stepper.Step
+                label="First step"
+                description="Enter payment info"
+                icon={<IconForms />}
+              ></Stepper.Step>
+              <Stepper.Step
+                label="Second step"
+                description="Customize your link"
+                icon={<IconWand />}
+              ></Stepper.Step>
+              <Stepper.Step
+                label="Share"
+                description="Share your link"
+                icon={<IconShare3 />}
+              ></Stepper.Step>
+              <Stepper.Completed>
+                Completed, click back button to get to previous step
+              </Stepper.Completed>
+            </Stepper>
+          </Container>
           <Container size="xs" padding="md">
-            <Form />
+            {active === 0 && (
+              <>
+                <Form
+                  formData={formData}
+                  handleChange={handleFormChange}
+                  handleNumberInputChange={handleFormNumberInputChange}
+                  errors={errors}
+                />
+                <Flex justify="center" align="center" mih="72">
+                  <Button
+                    rightSection={<IconChevronRight />}
+                    onClick={handleFormSubmit}
+                  >
+                    Generate QR Code
+                  </Button>
+                </Flex>
+              </>
+            )}
+            {active === 1 && <Customize data={formData} />}
           </Container>
         </AppShell.Main>
       </AppShell>
