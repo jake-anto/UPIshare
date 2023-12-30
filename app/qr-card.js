c19d5da7b7782b7d0f19bc645b8a7673f7324f1e
Jake Anto <64896514+jake-anto@users.noreply.github.com>
2023-12-30 21:01:06 +0000

    Refactor QrCard customization

--- app/qr-card.js
+++ app/qr-card.js
@@ -1,27 +1,73 @@
-import { ActionIcon, Avatar, Flex, Group, Paper, Text } from "@mantine/core";
-import { IconShare } from "@tabler/icons-react";
+import {
+  ActionIcon,
+  Avatar,
+  Button,
+  Flex,
+  Group,
+  Paper,
+  Text,
+} from "@mantine/core";
+import { IconCurrencyRupee, IconShare } from "@tabler/icons-react";
 import QRCode from "react-fancy-qrcode";
 
-export default function QrCard({ data, avatarColor, avatarInitials }) {
+function generateUpiUrl(data) {
+  let url = `upi://pay?pa=${data.id}&pn=${data.name}`;
+
+  if (data.amount) {
+    url += `&am=${data.amount}`;
+  }
+
+  if (data.note !== "") {
+    url += `&tn=${data.note}`;
+  }
+
+  return url;
+}
+
+export default function QrCard({ data, customizations }) {
+  const UpiUrl = generateUpiUrl(data);
+
   return (
     <Paper shadow="md" radius="xl" p="xl" maw="400px">
       <Group>
-        <Avatar color={avatarColor}>{avatarInitials}</Avatar>
-        <div style={{ flex: 1 }}>
-          <Text size="sm" fw={500}>
+        {customizations.avatar && (
+          <Avatar
+            color={
+              customizations.applyColorToAvatar
+                ? customizations.primaryColor
+                : ""
+            }
+          >
+            {customizations.avatarInitials}
+          </Avatar>
+        )}
+
+        <div style={{ flex: 1 }} >
+          <Text size="sm" fw={500} >
             {data.name}
           </Text>
           <Text c="dimmed" size="xs">
             {data.id}
           </Text>
         </div>
-        <ActionIcon variant="transparent" color={avatarColor}>
-          <IconShare />
-        </ActionIcon>
+        {customizations.shareButton && (
+          <>
+            <ActionIcon
+              variant="transparent"
+              color={
+                customizations.applyColorToShareButton
+                  ? customizations.primaryColor
+                  : ""
+              }
+            >
+              <IconShare />
+            </ActionIcon>
+          </>
+        )}
       </Group>
       <Flex justify="center" align="center" m="sm">
         <QRCode
-          value={data.id}
+          value={UpiUrl}
           size={175}
           color="#ffffff"
           backgroundColor="#242424"
@@ -29,6 +75,26 @@ export default function QrCard({ data, avatarColor, avatarInitials }) {
           positionRadius={["5%", "1%"]}
         />
       </Flex>
+      <Text align="center" size="xs">
+        Scan to pay
+      </Text>
+      {customizations.payButton && (
+        <>
+          <Flex align="center" m="sm">
+            <Button
+              leftSection={<IconCurrencyRupee />}
+              color={
+                customizations.applyColorToPayButton
+                  ? customizations.primaryColor
+                  : ""
+              }
+              fullWidth
+            >
+              Proceed to Pay
+            </Button>
+          </Flex>
+        </>
+      )}
     </Paper>
   );
 }
