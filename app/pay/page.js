15f7f9dcd79dbfb6791f9519ead0a7ba0f77de25
Jake Anto <64896514+jake-anto@users.noreply.github.com>
2023-12-31 13:35:07 +0000

    Fix Node.js compatibility issue

--- app/pay/page.js
+++ app/pay/page.js
@@ -5,44 +5,46 @@ import Header from "../header";
 import QrCard from "../qr-card";
 
 export default function Pay() {
-  const data = (function () {
+  // Node.js fix
+  if (typeof document != "undefined") {
     const params = new URLSearchParams(document.location.search);
-    return {
-      id: params.get("id"),
-      name: params.get("name"),
-      amount: params.get("amount"),
-      note: params.get("note"),
-    };
-  })();
-  const customizations = (function () {
-    const params = new URLSearchParams(document.location.search);
-    return {
-      primaryColor: params.get("primaryColor"),
-      avatarInitials: params.get("avatarInitials"),
-      shareButton: params.get("shareButton"),
-      payButton: params.get("payButton"),
-      avatar: params.get("avatar"),
-      applyColorToAvatar: params.get("applyColorToAvatar"),
-      applyColorToShareButton: params.get("applyColorToShareButton"),
-      applyColorToPayButton: params.get("applyColorToPayButton"),
-    };
-  })();
-  return (
-    <>
-      <AppShell header={{ height: 72 }} padding="md">
-        <AppShell.Header>
-          <Header />
-        </AppShell.Header>
-        <AppShell.Main>
-          <Flex justify="center" align="center" gap="md">
-            <QrCard
-              data={data}
-              customizations={customizations}
-              preview={false}
-            />
-          </Flex>
-        </AppShell.Main>
-      </AppShell>
-    </>
-  );
+    const data = (function () {
+      return {
+        id: params.get("id"),
+        name: params.get("name"),
+        amount: params.get("amount"),
+        note: params.get("note"),
+      };
+    })();
+    const customizations = (function () {
+      return {
+        primaryColor: params.get("primaryColor"),
+        avatarInitials: params.get("avatarInitials"),
+        shareButton: params.get("shareButton"),
+        payButton: params.get("payButton"),
+        avatar: params.get("avatar"),
+        applyColorToAvatar: params.get("applyColorToAvatar"),
+        applyColorToShareButton: params.get("applyColorToShareButton"),
+        applyColorToPayButton: params.get("applyColorToPayButton"),
+      };
+    })();
+    return (
+      <>
+        <AppShell header={{ height: 72 }} padding="md">
+          <AppShell.Header>
+            <Header />
+          </AppShell.Header>
+          <AppShell.Main>
+            <Flex justify="center" align="center" gap="md">
+              <QrCard
+                data={data}
+                customizations={customizations}
+                preview={false}
+              />
+            </Flex>
+          </AppShell.Main>
+        </AppShell>
+      </>
+    );
+  }
 }
